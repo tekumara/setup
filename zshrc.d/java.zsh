178aaaf33e9a0e798fc65e7a88185751d3ee487e
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-05-24 15:12:54 +1000

    default to jdk8

--- zshrc.d/java.zsh
+++ zshrc.d/java.zsh
@@ -1,3 +1,6 @@
 alias jdks='/usr/libexec/java_home -V'
 alias jdk8='export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home'
 alias jdk11='export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home'
+
+# default to jdk8 because of Spark
+jdk8
