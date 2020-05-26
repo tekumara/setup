ed6ac5818f56ad758ad772a84a40144900c4e096
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-05-26 22:23:22 +1000

    more git aliases

--- zshrc.d/git.zsh
+++ zshrc.d/git.zsh
@@ -1,8 +1,11 @@
-# git aliases
+# scmpuff git aliases
 eval "$(scmpuff init -s)"
 
+# taken from scm breeze https://github.com/scmbreeze/scm_breeze/blob/master/lib/git/aliases.sh
 alias gc='git commit'
 alias gcm='git commit --amend'
 alias gb='git branch'
 alias gpl='git pull'
 alias gps='git push'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gla='gl --all'
