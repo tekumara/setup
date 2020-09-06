20ad59c417a197a4f77f7c829e4ddf7c550860e5
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-09-06 19:51:27 +1000

    update docker aliases

--- dotfiles/.zshrc.d/docker.plugin.zsh
+++ dotfiles/.zshrc.d/docker.plugin.zsh
@@ -1,5 +1,6 @@
 alias dr='docker run --rm -it'
 alias drb='docker run --rm -it --entrypoint /bin/bash'
-alias drbv='docker run -it --entrypoint /bin/bash  -v "$(pwd)":/app -w /app'
+alias drv='docker run --rm -it -v "$(pwd)":/app -w /app'
+alias drvn='docker run -it -v "$(pwd)":/app -w /app'
 alias ds='docker start -i'
 deb() { docker exec -it "$1" bash }
