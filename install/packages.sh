19664f2a0f361dba534a96f4d265b9b9cef44b62
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-09-20 19:34:54 +1000

    packages - don't install pyright globally

--- install/packages.sh
+++ install/packages.sh
@@ -34,7 +34,6 @@ go get -u github.com/go-delve/delve/cmd/dlv
 # python tools
 # NB: pipx installed packages use the system version of python
 pipx install py-spy
-npm install -g pyright
 
 # for formatting markdown, json and javscript
 npm install -g prettier
