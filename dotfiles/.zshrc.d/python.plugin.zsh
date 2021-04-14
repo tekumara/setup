00a5cf5d897537dcd3a099693688256260dde078
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-04-14 17:57:07 +1000

    update aliases to use .venv/

--- dotfiles/.zshrc.d/python.plugin.zsh
+++ dotfiles/.zshrc.d/python.plugin.zsh
@@ -2,10 +2,10 @@
 # pipx packages aren't broken when brew upgrades python
 export PIPX_DEFAULT_PYTHON=$(pyenv which python)
 
-# activate virtualenv in ./venv/
-alias venv='. venv/bin/activate'
+# activate virtualenv in .venv/ or venv/
+alias venv='{[[ -d .venv ]] && . .venv/bin/activate} || {[[ -d venv ]] && . venv/bin/activate} || echo "Missing .venv/"'
 # create venv
-alias mkvenv='python3 -m venv --clear venv'
+alias mkvenv='python3 -m venv --clear .venv'
 
 # prevent pip from installing globally
 export PIP_REQUIRE_VIRTUALENV=true
