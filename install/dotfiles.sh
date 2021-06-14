e4fb31e35dc2fb1c3182109a8d27182e8fa2ceec
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-06-14 17:28:16 +1000

    feat: run zsh on install

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -16,3 +16,7 @@ if [[ ! -d ~/.vim_runtime ]]; then
     git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
     sh ~/.vim_runtime/install_basic_vimrc.sh
 fi
+
+# run zsh to start antibody for the first time to download plugins
+# its safe to ignore the "(eval):1: can't change option: zle" error
+zsh -i -c exit;
