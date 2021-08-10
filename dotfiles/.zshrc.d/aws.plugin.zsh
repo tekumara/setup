272837c7b078769f7e25a90652ba9ba8104cf1aa
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-08-10 21:50:42 +1000

    feat: use aws-vault instead of awsweb
    [aws-vault](https://github.com/glassechidna/awsweb) is now used for command line AWS access and has the following advantages over other options (eg: awscli, awsweb, awsume):
    
    - stores AWS credentials securely in the macOS keychain rather than in plaintext on disk
    - only asks for an MFA token once every 8 hours
    - can assume roles using short-term (STS) credentials
    - provides easy AWS access key rotation (no need to login to the console)
    - can open a browser tab and sign you into the AWS console
    - provides terminal and graphical prompts (useful when called from vscode) for MFA tokens
    - supports role session names (makes it easier to audit API calls in CloudTrail)

--- dotfiles/.zshrc.d/aws.plugin.zsh
+++ dotfiles/.zshrc.d/aws.plugin.zsh
@@ -1,10 +1,28 @@
-alias ab='awsweb browser'
+# use the login keychain for aws-vault so we don't need a separate password
+export AWS_VAULT_KEYCHAIN_NAME=login
 
-awsenv() {
-  eval $(awsweb env "$@")
+# print AWS env vars as export statements
+aws_vault_export() {
+   aws-vault exec "$1" -- env | grep AWS | sed -e 's/^/export\ /'
 }
 
-alias ae='awsenv'
+ae() {
+   if [[ "$1" == "--unset" ]]; then
+      # unset AWS_* vars, ie: unassume any roles (zsh only)
+      unset $(print -rC1 - $parameters[(I)AWS*] | egrep -v 'AWS_PAGER|AWS_VAULT')
+   else
+      # eval AWS env vars in current shell
+      # ignore AWS_PAGER because it has spaces
+      # ignore AWS_VAULT so we can switch to other roles in the same shell
+      eval $(aws_vault_export "$1" | egrep -v 'AWS_PAGER|AWS_VAULT')
+   fi
+}
+
+ab() {
+   aws-vault login "$1"
+}
+
+alias awsid='aws sts get-caller-identity'
 
 alias ec2='COLUMNS=$COLUMNS aec ec2'
 alias ami='COLUMNS=$COLUMNS aec ami'
