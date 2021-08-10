0d1edc5ab7de3bcbf1a1af5c03e18c7a2aa58774
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-08-10 21:49:56 +1000

    feat: mkenv - activate venv after creation

--- dotfiles/.zshrc.d/python.plugin.zsh
+++ dotfiles/.zshrc.d/python.plugin.zsh
@@ -5,8 +5,8 @@ export PIPX_DEFAULT_PYTHON
 
 # activate virtualenv in .venv/ or venv/
 alias venv='{[[ -d .venv ]] && . .venv/bin/activate} || {[[ -d venv ]] && . venv/bin/activate} || echo "Missing .venv/"'
-# create venv
-alias mkvenv='virtualenv .venv'
+# create venv and activate
+alias mkvenv='virtualenv .venv && . .venv/bin/activate'
 
 # prevent pip from installing globally
 export PIP_REQUIRE_VIRTUALENV=true
