09e10ec7ce28b2cdd22a2cb4ca3145688b0117ff
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-08-13 18:47:39 +1000

    java 11

--- dotfiles/.zshrc.d/java.plugin.zsh
+++ dotfiles/.zshrc.d/java.plugin.zsh
@@ -1,6 +1,5 @@
 alias jdks='/usr/libexec/java_home -V'
-alias jdk8='export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home'
 alias jdk11='export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home'
 
-# default to jdk8 because of Spark
-jdk8
+# default to jdk11 for Spark 3.x
+jdk11
