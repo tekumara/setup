283a98a06b83ba58ae090f9782bbe081685f868f
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-08-13 20:06:18 +1000

    git aliases: h instead of gh

--- dotfiles/.zshrc.d/git.plugin.zsh
+++ dotfiles/.zshrc.d/git.plugin.zsh
@@ -32,10 +32,14 @@ alias ginit='git init && git commit -m "root commit" --allow-empty'
 alias gbu='current=$(git rev-parse --abbrev-ref HEAD) && git branch --set-upstream-to="origin/$current" "$current"'
 # if a main branch exists, checkout main, else checkout master
 alias gcom='if git show-ref --verify --quiet refs/heads/main; then git checkout main; else git checkout master; fi'
+# delete branch
+gbd() {
+    git branch -D "$1"; git push origin --delete "$1"
+}
 
 # github cli
-alias ghb='gh browse --branch $(git rev-parse HEAD)'
-alias ghprc='gh pr create --fill -w'
+alias hb='gh browse --branch $(git rev-parse HEAD)'
+alias hprc='gh pr create --fill -w'
 
 # forgit aliases take precedence over anything above
 alias ga=forgit::add
