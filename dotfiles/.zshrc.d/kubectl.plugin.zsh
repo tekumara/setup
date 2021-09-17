397ef17aa9c8d5aac542418158573cac936a8b99
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-09-17 15:52:29 +1000

    update kubectl plugin

--- dotfiles/.zshrc.d/kubectl.plugin.zsh
+++ dotfiles/.zshrc.d/kubectl.plugin.zsh
@@ -3,3 +3,5 @@
 # https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/kubectl/kubectl.plugin.zsh
 # https://github.com/ahmetb/kubectl-aliases
 alias k='kubectl'
+
+export KUBECONFIG=$HOME/.kube/config:$HOME/.flyte/k3s/k3s.yaml:$HOME/.k3d/kubeconfig-gha.yaml
