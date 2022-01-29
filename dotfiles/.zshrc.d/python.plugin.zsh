a9bce934720718ac388b3c5331a3931cb1d6737e
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-01-29 16:57:52 +1100

    feat: debugpy alias

--- dotfiles/.zshrc.d/python.plugin.zsh
+++ dotfiles/.zshrc.d/python.plugin.zsh
@@ -9,4 +9,11 @@ export PIPX_DEFAULT_PYTHON
 # activate virtualenv in .venv/ or venv/
 alias venv='{[[ -d .venv ]] && . .venv/bin/activate} || {[[ -d venv ]] && . venv/bin/activate} || echo "Missing .venv/"'
 # create venv and activate
-alias mkvenv='virtualenv .venv && . .venv/bin/activate'
+alias mkvenv='virtualenv --clear .venv && . .venv/bin/activate'
+
+# vscode debugger
+debugpy() {
+    local port=62888
+    echo "Attach vscode debugger to port $port" >&2
+    python -m debugpy --listen "$port" --wait-for-client "$@"
+}
