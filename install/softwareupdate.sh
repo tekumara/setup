8429490aae9cec171d780247ddb555cb40d3dbaf
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-06-10 21:46:02 +1000

    feat: skip system updates during ci

--- install/softwareupdate.sh
+++ install/softwareupdate.sh
@@ -4,6 +4,12 @@
 
 set -euo pipefail
 
+if ${CI:-false}; then
+    # avoid downloading new versions of macOS which takes ~20mins
+    echo "Skipping system updates during CI"
+    exit 0
+fi
+
 # do a check with no-scan first for speed
 if ! softwareupdate --no-scan -l 2>&1 | grep "No new software available"; then
     # install recommended system updates (eg: xcode tools)
