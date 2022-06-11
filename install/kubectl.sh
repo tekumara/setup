8c44b7550e27ca929c680e8965606263737c2324
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-06-11 12:05:16 +1000

    feat: skip downloading kubectl version if already installed

--- install/kubectl.sh
+++ install/kubectl.sh
@@ -12,15 +12,11 @@ if [[ "$(stat -f '%u' /usr/local/bin)" != "$(id -u)" ]]; then
     set +x
 fi
 
-# remove versions installed prior
 if [[ -f /usr/local/bin/kubectl && "$(stat -f '%u' /usr/local/bin/kubectl)" == "0" ]]; then
-    # installed as root by Docker so use sudo to remove
+    # previously installed as root by the docker cask so chown
     set -x
-    sudo rm -f /usr/local/bin/kubectl
+    sudo chown "$(whoami)" /usr/local/bin/kubectl
     set +x
-else
-    # installed as current user by brew or doesn't exist
-    rm -f /usr/local/bin/kubectl
 fi
 
 # specific kubectl version, should be within +/- 1 version of the server
@@ -28,10 +24,19 @@ v=v1.21.12
 
 if [[ "$(/usr/bin/uname -m)" == "arm64" ]]; then
     # Apple Silicon (M1 Mac)
-    curl -fsSLo /usr/local/bin/kubectl "https://dl.k8s.io/release/$v/bin/darwin/arm64/kubectl"
-    echo "d518a7642874d7c6ca863b12bb2ce591840c1798b460f1f97b1eea7fbb41b9c9  /usr/local/bin/kubectl"  | shasum --check
+    sha256=d518a7642874d7c6ca863b12bb2ce591840c1798b460f1f97b1eea7fbb41b9c9
+    url="https://dl.k8s.io/release/$v/bin/darwin/arm64/kubectl"
+
 else
-    curl -fsSLo /usr/local/bin/kubectl "https://dl.k8s.io/release/$v/bin/darwin/amd64/kubectl"
-    echo "014d92af39ea8d3e57f01581c9ce44ea2b107c55d8cdeb838494014d034a6c17  /usr/local/bin/kubectl"  | shasum --check
+    sha256=014d92af39ea8d3e57f01581c9ce44ea2b107c55d8cdeb838494014d034a6c17
+    url="https://dl.k8s.io/release/$v/bin/darwin/amd64/kubectl"
 fi
-chmod a+x /usr/local/bin/kubectl
+
+# check any existing binary first, so we can skip if the desired version is already installed
+if ! [[ -f /usr/local/bin/kubectl ]] || ! echo "$sha256  /usr/local/bin/kubectl"  | shasum -s --check; then
+    set -x
+    curl -fsSLo /usr/local/bin/kubectl $url
+    echo "$sha256  /usr/local/bin/kubectl"  | shasum --check
+    chmod a+x /usr/local/bin/kubectl
+    set +x
+fi
