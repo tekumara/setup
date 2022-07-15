196a7f687ff4eabe716fad2650cb0f8c88accf9d
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-07-15 22:03:58 +1000

    fix: fnm use
    fnm use now changes the version in the current shell

--- dotfiles/.zshrc.d/node.plugin.zsh
+++ dotfiles/.zshrc.d/node.plugin.zsh
@@ -1,5 +1,1 @@
-export PATH=$HOME/.fnm/current/bin:$PATH
-export FNM_MULTISHELL_PATH=$HOME/.fnm/current
-export FNM_DIR=$HOME/.fnm
-export FNM_NODE_DIST_MIRROR=https://nodejs.org/dist
-export FNM_LOGLEVEL=info
+eval "$(fnm env --use-on-cd)"
