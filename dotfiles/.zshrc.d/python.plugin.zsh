8e59dcbb8a58ae9cd75e5176b3722b25147fbf99
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-07-24 16:55:53 +1000

    feat: python - faster startup and install debupy if missing

--- dotfiles/.zshrc.d/python.plugin.zsh
+++ dotfiles/.zshrc.d/python.plugin.zsh
@@ -3,7 +3,8 @@ export PIP_REQUIRE_VIRTUALENV=true
 
 # use a stable python3 path instead of the default brew path so
 # pipx packages aren't broken when brew upgrades python
-PIPX_DEFAULT_PYTHON=$(pyenv which python)
+# NB: we don't use `pyenv which python` here because it's slooow (200ms)
+PIPX_DEFAULT_PYTHON="$HOME/.pyenv/versions/$(cat ~/.pyenv/version)/bin/python"
 export PIPX_DEFAULT_PYTHON
 
 # activate virtualenv in .venv/ or venv/
@@ -14,6 +15,7 @@ alias mkvenv='virtualenv --clear .venv && . .venv/bin/activate'
 # vscode debugger
 debugpy() {
     local port=62888
+    [[ -d $VIRTUAL_ENV/lib/python*/site-packages/debugpy(#q) ]] || (echo "Installing debugpy" && pip install debugpy)
     echo "Attach vscode debugger to port $port" >&2
     python -m debugpy --listen "$port" --wait-for-client "$@"
 }
