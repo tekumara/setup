a13251cb6e8910e70d6a8e40c85629b3baa46658
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-01-18 11:50:55 +1100

    Revert "feat: generate ~/.passage/identities"
    This reverts commit c848621520e987911e0d6f5bf10b1d70dfa17f33.
    
    not needed because identities will be on cloud storage anyway

--- install/passage.sh
+++ install/passage.sh
@@ -11,18 +11,3 @@ cd "$tmpdir"
 make install PREFIX="$(brew --cellar)/passage/$(git describe --tags)" WITH_ZSHCOMP=yes
 brew link passage
 
-if [[ ! -e ~/.passage/identities ]]; then
-    echo -e "Insert all yuibkeys and press any key to continue...\n"
-    read -n1 -s -r
-
-    tmpfile="$(mktemp -t "identities.XXXXXX")"
-    age-plugin-yubikey -i > "$tmpfile"
-    if [[ ! -s $tmpfile ]]; then
-        echo -e ERROR: "No yubikey age identities, are your keys connected and configured?" >&2
-        exit 42
-    fi
-
-    mv "$tmpfile" ~/.passage/identities
-
-    echo -e "\nSaved to ~/.passage/identities"
-fi
