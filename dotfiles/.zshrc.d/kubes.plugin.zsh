5ea670f63450cd492edc7c0684b1b8650c41bb06
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-04-04 18:23:29 +1000

    wip: kc-default

--- dotfiles/.zshrc.d/kubes.plugin.zsh
+++ dotfiles/.zshrc.d/kubes.plugin.zsh
@@ -1,5 +1,4 @@
-# just the minimal set I actually use
-# see also:
+# a minimal set of aliases adapted from
 # https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/kubectl/kubectl.plugin.zsh
 # https://github.com/ahmetb/kubectl-aliases
 alias k='kubectl'
@@ -37,10 +36,15 @@ krs() {
   kubectl run "$1" -it --image="$2" --command -- /bin/sh
 }
 
-# unset is needed becuase we are appending values below and
+# unset is needed because we are appending values below and
 # it may already have been set by the parent process (eg: vscode)
 unset KUBECONFIG
 
+# set KUBECONFIG to all config files in the paths below, so
+# we don't have to merge them into a single file
+#
+# use glob qualifer N (ie: null_glob) to prevent a "no matches found"
+# error if the glob doesn't match any files
 files=($HOME/.kube/*.yaml(N) $HOME/.k3d/kubeconfig*.yaml(N))
 for file in $files; do
   KUBECONFIG+="${KUBECONFIG+:}${file}"
@@ -54,3 +58,18 @@ apiVersion: v1
 kind: Config
 current-context: ""
 EOF
+
+kc-default() {
+  [[ "$#" -ne 1 ]] && echo -e "Usage: $0 default-context\n\neg: $0 docker-desktop" >&2 && return 42
+  # set default context by creating a file
+  # that appears first when ~/.kube is listed (see above)
+  # if no default is set then it will be the current-context as defined
+  # in the first file sorted alphanumerically
+  default_config="$HOME/.kube/00_config.yaml"
+    echo "Set default context in $default_config"
+    cat <<EOF >"${default_config}"
+apiVersion: v1
+kind: Config
+current-context: "$1"
+EOF
+}
