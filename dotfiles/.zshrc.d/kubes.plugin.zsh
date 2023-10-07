c2bc57789bd3c5a3fb9666fb385fc71ac82b9e09
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-10-07 16:27:15 +1100

    fix: correctly template kubeconfig file location
    now
    
    /var/folders/5y/hhvf46cx393_j75q4gm7tvzw0000gq/T/kubectx.gnhgxcoD
    
    instead of
    
    /var/folders/5y/hhvf46cx393_j75q4gm7tvzw0000gq/T/kubectx.XXXXXX.BRjfYpk6

--- dotfiles/.zshrc.d/kubes.plugin.zsh
+++ dotfiles/.zshrc.d/kubes.plugin.zsh
@@ -69,7 +69,7 @@ for file in $files; do
 done
 
 # kube context per shell https://github.com/ahmetb/kubectx/issues/12#issuecomment-557852519
-file="$(mktemp -t "kubectx.XXXXXX")"
+file="$(mktemp ${TMPDIR:-/tmp/}kubectx.XXXXXXXX)"
 export KUBECONFIG="${file}:${KUBECONFIG}"
 cat <<EOF >"${file}"
 apiVersion: v1
