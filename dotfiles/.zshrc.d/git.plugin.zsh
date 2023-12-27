f9c7c1dfa151d933e49a3236369d546157620f55
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-12-28 09:31:45 +1100

    feat: add wiggle

--- dotfiles/.zshrc.d/git.plugin.zsh
+++ dotfiles/.zshrc.d/git.plugin.zsh
@@ -76,3 +76,14 @@ alias hprv='gh pr view --web'
 alias hwv='gh workflow view ci.yml'
 # view ci.yml workflow in the browser
 alias hwvb='gh workflow view -w ci.yml'
+
+wiggle-all() {
+    # apply all rej files
+    for rej in *.rej .*.rej(N); do
+        base="$(basename -s .rej "$rej")"
+        echo "Applying $rej to $base"
+        wiggle --replace "$base" "$rej"
+        rm "$rej"
+        rm "$base.porig"
+    done
+}
