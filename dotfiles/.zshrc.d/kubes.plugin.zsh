20891da93edc0d73be5c22ea4b3239144725c6f9
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-06-07 14:32:11 +1000

    feat(kubes): alias kpf

--- dotfiles/.zshrc.d/kubes.plugin.zsh
+++ dotfiles/.zshrc.d/kubes.plugin.zsh
@@ -83,3 +83,34 @@ kc-default() {
   # so the current-context setting is stored in the default config file
   KUBECONFIG="${default_config}:${KUBECONFIG}" kubectx
 }
+
+kpf() {
+  set -euo pipefail
+  set -x
+
+  [[ "$#" -eq 0 ]] && echo -e "Start/stop kubectl port-forward whilst running a command.\n\nUsage: $0 TYPE/NAME [LOCAL_PORT:]REMOTE_PORT command [args..]" >&2 && exit 42
+
+  die() {
+      echo -e ERROR: "$@" >&2
+      cleanup 42
+  }
+
+  cleanup() {
+      local exit_status="$?"
+      kill "$kubectl_pid"
+      wait "$kubectl_pid" 2> /dev/null || true
+      echo "kubectl port-forward stopped" >&2
+      set +x
+      return "${exit_status}"
+  }
+
+  kubectl port-forward "$1" "$2" >&2 &
+  kubectl_pid=$!
+  echo kubectl port-forward "$1" "$2" >&2
+
+  trap 'cleanup' ERR
+  timeout 5 sh -c "until nc -z localhost ${2%:*} ; do sleep 0.1; done" || die "timed out"
+  shift 2
+  "$@"
+  cleanup
+}
