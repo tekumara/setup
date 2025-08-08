d3298f30c2659a885456239a3431cdca53788dbb
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-08-08 21:32:12 +1000

    dinstall: fix
    ❯ dinstall https://github.com/jj-vcs/jj/releases/download/v0.32.0/jj-v0.32.0-aarch64-unknown-linux-musl.tar.gz /usr/local/bin/jj
    Downloading https://github.com/jj-vcs/jj/releases/download/v0.32.0/jj-v0.32.0-aarch64-unknown-linux-musl.tar.gz
    49798a0be2cab38283823db7a053f19e73dabc47 /tmp/jj-v0.32.0-aarch64-unknown-linux-musl.tar.gz
    tar: .: Cannot utime: Operation not permitted
    tar: .: Cannot change mode to rwxr-xr-t: Operation not permitted
    tar: Exiting with failure status due to previous errors

--- dotfiles/.zshrc.d/dinstall.plugin.zsh
+++ dotfiles/.zshrc.d/dinstall.plugin.zsh
@@ -2,6 +2,8 @@ dinstall() {
     local url=$1
     local dest=$2
     local sha256=$3
+    local tmpdir
+    tmpdir="$(mktemp -d)"
 
     { [[ -z "$url" ]] || [[ -z "$dest" ]] ;} && {
         echo "Usage: $0 url dest [sha256]"
@@ -11,7 +13,7 @@ dinstall() {
     # check sha of any existing binary first, and skip if already installed
     # if the url is an archive, this will always fail and we'll download
     if ! [[ -f "$dest" ]] || [[ -z "$sha256" ]] || { ! echo "$sha256  $dest"  | shasum -s --check ;} then
-        download=/tmp/$(basename "$url")
+        download="${tmpdir}"/$(basename "$url")
         echo "Downloading $url"
         curl -fsSLo "$download" "$url"
         if [[ -n "$sha256" ]]; then
@@ -22,10 +24,10 @@ dinstall() {
 
         case "$download" in
             # if download is a tar archive, then assume $(basename "$dest") is the desired file within the archive
-            # ** will find the extracted file in subdirs too (eg: /tmp/gh_2.40.1_linux_amd64/bin/gh) - for globbing
+            # ** will find the extracted file in subdirs too (eg: "${tmpdir}"/gh_2.40.1_linux_amd64/bin/gh) - for globbing
             # to work this needs to be an array, hence the extra ( )
-            *.tar.gz) tar -zxf "$download" -C /tmp && file=(/tmp/**/$(basename "$dest")) ;;
-            *.zip) unzip "$download" -d /tmp && file=(/tmp/**/$(basename "$dest")) ;;
+            *.tar.gz) tar -zxf "$download" -C "${tmpdir}/" && file=("${tmpdir}"/**/$(basename "$dest")) ;;
+            *.zip) unzip "$download" -d "${tmpdir}/" && file=("${tmpdir}"/**/$(basename "$dest")) ;;
             *)        file=$download ;;
         esac
 
