355d3227f2d6fd31cf31ef99b2f57e2ce71a52ab
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-12-16 10:30:07 +1100

    nvim: upgrade plugins + experiment with root_dir for typos-lsp

--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -6,9 +6,9 @@ return {
         typos_lsp = {
           -- typos-lsp must be on your PATH, or otherwise change this to an absolute path to typos-lsp
           -- defaults to typos-lsp if unspecified
-          cmd = { "/Users/oliver.mannion/code/typos-lsp/target/debug/typos-lsp" },
+          -- cmd = { "/Users/oliver.mannion/code/typos-lsp/target/debug/typos-lsp" },
           -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
-          cmd_env = { RUST_LOG = "error" },
+          cmd_env = { RUST_LOG = "debug" },
           init_options = {
               -- Custom config. Used together with a config file found in the workspace or its parents,
               -- taking precedence for settings declared in both.
@@ -17,7 +17,21 @@ return {
               -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
               -- Defaults to Info.
               diagnosticSeverity = "Info"
-          }
+          },
+          root_dir = function(bufnr, on_dir)
+            -- 1. Try to find a project marker (typos.toml, pyproject.toml, etc.)
+            local root = vim.fs.root(bufnr, { "typos.toml", "_typos.toml", ".typos.toml", "pyproject.toml", "Cargo.toml", "go.mod" })
+
+            -- 2. Fallback to the directory argument if nvim was started with one
+            if not root then
+              local arg = vim.fn.argv(0)
+              if arg ~= "" and vim.fn.isdirectory(arg) == 1 then
+                root = vim.fn.fnamemodify(arg, ":p")
+              end
+            end
+
+            on_dir(root)
+          end,
         },
       },
     },
