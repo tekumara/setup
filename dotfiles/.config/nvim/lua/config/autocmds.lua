0b8a61531bf5b157117f59f3b93b652f48ee2a1d
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-12-16 10:30:07 +1100

    nvim: use typos-lsp for spellchecking

--- dotfiles/.config/nvim/lua/config/autocmds.lua
+++ dotfiles/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,7 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- disable wrapping and spell checking for text files because we use typos-lsp for spelling
+-- see https://github.com/LazyVim/LazyVim/blob/c64a61734fc9d45470a72603395c02137802bc6f/lua/lazyvim/config/autocmds.lua#L97
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
---
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        typos_lsp = {
+          -- typos-lsp must be on your PATH, or otherwise change this to an absolute path to typos-lsp
+          -- defaults to typos-lsp if unspecified
+          cmd = { "/Users/oliver.mannion/code/typos-lsp/target/debug/typos-lsp" },
+          -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
+          cmd_env = { RUST_LOG = "error" },
+          init_options = {
+              -- Custom config. Used together with a config file found in the workspace or its parents,
+              -- taking precedence for settings declared in both.
+              -- Equivalent to the typos `--config` cli argument.
+              -- config = '~/code/typos-lsp/crates/typos-lsp/tests/typos.toml',
+              -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
+              -- Defaults to Info.
+              diagnosticSeverity = "Info"
+          }
+        },
+      },
+    },
+  }
+}
