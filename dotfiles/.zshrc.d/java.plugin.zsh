9ae9cb6c5f829e9a369485d4a8b5ae895cb20a57
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2026-08-12 22:15:14 +1000

    java: remove java-plugin.sh

--- dotfiles/.zshrc.d/java.plugin.zsh
+++
@@ -1,5 +0,0 @@
-alias jdks='/usr/libexec/java_home -V'
-alias jdk11='export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-11.jdk/Contents/Home'
-
-# default to jdk11 to align with EMR/Lambda/Spark
-jdk11
