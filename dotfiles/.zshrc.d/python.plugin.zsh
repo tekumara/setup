54cc3f71affad14b5d1a3d9f51a4e7629936aa8b
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2026-08-19 21:16:56 +1000

    python: allow debugpy outside virtualenv

--- dotfiles/.zshrc.d/python.plugin.zsh
+++ dotfiles/.zshrc.d/python.plugin.zsh
@@ -16,18 +16,14 @@ mktmpenv() {
 # vscode debugger
 debugpy() {
     local port=62888
-    if [[ -z $VIRTUAL_ENV ]]; then
-        echo "Activate your virtualenv first to avoid module import errors."
-    else
-        local debugpy=$(ls -dtr ~/.vscode/extensions/ms-python.debugpy-*/bundled/libs/debugpy | head -n 1)
-        # enable global qualifier syntax ie:(#q...) see https://zsh.sourceforge.io/Doc/Release/Expansion.html#Glob-Qualifiers
-        # this is set locally for this function only
-        setopt localoptions extendedglob
-        # use glob qualifier N (ie: null_glob) so that if the glob has no matches we don't die
-        [[ -d $~debugpy(#qN) ]] || { echo "vscode python extension not installed" && return }
-        echo "Attach vscode debugger to port $port" >&2
-        uv run python $~debugpy --listen "$port" --wait-for-client "$@"
-    fi
+    local debugpy=$(ls -dtr ~/.vscode/extensions/ms-python.debugpy-*/bundled/libs/debugpy | head -n 1)
+    # enable global qualifier syntax ie:(#q...) see https://zsh.sourceforge.io/Doc/Release/Expansion.html#Glob-Qualifiers
+    # this is set locally for this function only
+    setopt localoptions extendedglob
+    # use glob qualifier N (ie: null_glob) so that if the glob has no matches we don't die
+    [[ -d $~debugpy(#qN) ]] || { echo "vscode python extension not installed" && return }
+    echo "Attach vscode debugger to port $port" >&2
+    uv run python $~debugpy --listen "$port" --wait-for-client "$@"
 }
 
 pyright() {
