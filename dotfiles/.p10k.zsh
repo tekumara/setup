e9561fa82e9b5624c5dfc4f09110a7dc4c689d12
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2026-08-19 21:16:56 +1000

    prompt: disable gcloud segment
    Avoid running Powerlevel10k\u2019s synchronous gcloud configuration prefetch.
    The profile recorded _p9k_gcloud_prefetch at 3,768.54 ms (79.52%
    of 4,739.44 ms total); with the cached path it took 0.53 ms.
    Disabling the segment removes that occasional multi-second prompt block.

--- dotfiles/.p10k.zsh
+++ dotfiles/.p10k.zsh
@@ -83,7 +83,7 @@
     aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
     aws_eb_env              # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
     azure                   # azure account name (https://docs.microsoft.com/en-us/cli/azure)
-    gcloud                  # google cloud cli account and project (https://cloud.google.com/)
+    # gcloud                # disabled: synchronous prefetch can block the prompt
     google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
     toolbox                 # toolbox name (https://github.com/containers/toolbox)
     context                 # user@hostname
