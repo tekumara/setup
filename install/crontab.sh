ffe5988fb3a1582ad7daaf131ae4a21baab7aa6e
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2026-09-11 21:39:43 +1000

    fix: install crontab on fresh machine

--- install/crontab.sh
+++ install/crontab.sh
@@ -7,4 +7,6 @@ set -euo pipefail
 # Replace ~ with actual home path and install as crontab
 sed "s|~|$HOME|g" "$(dirname "$0")/crontab" | crontab -
 
+mkdir -p "$HOME/.zsh_history_backup"
+
 echo "Crontab installed"
